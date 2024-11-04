7e58f123aef8704aab65811c1f37a6b3dccb754d
wocks95 <rla1995cks@naver.com>
2024-11-04 14:11:56 +0900

    20241104

--- 01_start/src/start/Start.java
+++ 01_start/src/start/Start.java
@@ -17,7 +17,7 @@ public class Start {
      * multi-line comment
     */
     // Console 출력 방식
-    System.out.print(1);          // 1 출력
+    System.out.print(1);          // 1 출력함
     System.out.println(2);        // 2 출력 후 줄 바꿈
     System.out.printf("%d\n", 3); // 3 출력 후 줄바꿈
   }
