a17b60b08eccdec0104b089ec2732d1eedbf6930
wocks95 <rla1995cks@naver.com>
2024-11-04 13:12:07 +0900

    20241104

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,25 @@
+package start;
+
+/*
+ * Documentation comment (class)
+ * 작성자, 작성일자, 버전, 설명 등을 API 문서로 제공하기 위한 주석
+ */
+public class Start {
+  /*
+   *Documentation comment (method)
+   
+  */
+  public static void main(String[] args) {
+
+    // cingle-line comment
+    
+    /*
+     * multi-line comment
+    */
+    // Console 출력 방식
+    System.out.print(1);          // 1 출력
+    System.out.println(2);        // 2 출력 후 줄 바꿈
+    System.out.printf("%d\n", 3); // 3 출력 후 줄바꿈
+  }
+
+}
