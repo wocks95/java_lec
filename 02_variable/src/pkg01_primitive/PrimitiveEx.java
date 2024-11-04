ace5a4b76c60a4df4424ba665ea8d689bffda555
wocks95 <rla1995cks@naver.com>
2024-11-04 14:55:45 +0900

    2041104

--- 02_variable/src/pkg01_primitive/PrimitiveEx.java
+++ 02_variable/src/pkg01_primitive/PrimitiveEx.java
@@ -16,7 +16,7 @@ public class PrimitiveEx {
     short b = 32767;
     System.out.println(b);
     
-    // 3. ★★★int : 4바이트 정수 - 기본 정수 타입
+    // 3. ★★★int (integer) : 4바이트 정수 - 기본 정수 타입
     int c = 2147483647;
     System.out.println(c);
     
@@ -24,7 +24,7 @@ public class PrimitiveEx {
     long d = 2147483648L; //long 타입의 값은 마지막에 L(l) 을 추가 (int 범위를 벗어난 값은 필수, 아니면 생략 가능)
     System.out.println(d);
     
-    // 5. float : 4바이트 실수 - 사용 안 함
+    // 5. float (floating point) : 4바이트 실수 - 사용 안 함
     float e = 1.111111111F; // float 타입의 값은 마지막에 F(f)를 추가
     System.out.println(e);
     
@@ -32,11 +32,11 @@ public class PrimitiveEx {
     double f = 1.111111111;
     System.out.println(f);
     
-    // 7. char : 2바이트 단일문자 - ASCII 코드(1바이트), 유니코드(2바이트)
+    // 7. ★char (character) : 2바이트 단일문자 - ASCII 코드(1바이트), 유니코드(2바이트)
     char g = '홍'; // 작은 따옴표, 영문/한글 모두 가능
     System.out.println(g);
     
-    // 8. boolean : 이론상 1비트(실제는 JVM이 담당) , true 또는 false 저장
+    // 8. ★boolean : 이론상 1비트(실제는 JVM이 담당) , true 또는 false 저장
     boolean h = true;
     System.out.println(h);
   }
