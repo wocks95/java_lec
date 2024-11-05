d04396656363f1ec78a2537e69b3ff068c97db83
wocks95 <rla1995cks@naver.com>
2024-11-05 17:58:45 +0900

    20241105

--- 05_array/src/pkg03_one_dimention_ex/Ex04.java
+++ 05_array/src/pkg03_one_dimention_ex/Ex04.java
@@ -27,13 +27,13 @@ public class Ex04 {
     
     
     int i = 0;
-    while(number > 0) {
-      binary[i++] = number % 2; //i++는 증가가 된 후에 그 다음에 멈춘다.
-      number /= 2; // number = number / 2; 
+    while(number > 0) {  // 종료 조건보다 넘지 않게 하기 위해 막음
+      binary[i++] = number % 2; //i++는 선 출력 후 증가이기때문에 완료된 시점인 그 다음에 멈춘다. i[5]에 안 멈추고 i[6]에 멈춤
+      number /= 2; // number = number / 2; // 나눈 몫을 또 나누기 위해서
     }
     
     for(int j = i - 1; j >= 0; j--) {
-      System.out.print(binary[j]);
+      System.out.print(binary[j]);    // System.out.print(); 가로로 나옴. 뒤에 println();이면 세로로 나옴
     }
     
     
